5b2c8a09b3a0bd003c606d68f77ef8e691dd9b54
R-G-Krishna <gopikrishna3211999@gmail.com>
2019-09-27 11:09:35 +0530

    this is my first commit

---
+++ 09-09-2019/tables using functions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace tables using functions
+{
+   public class main_class
+   {
+      static System.Random random_generator = new System.Random();
+      public static void func (
+             ??_Variable a)
+      {
+         ?? i;
+      
+         i =1;
+         while (!(i>10))
+         {
+            Console.WriteLine(a+"*"+i+"="+(a*i));
+            i =i+1;
+         }
+      }
+      public static void Main(string[] args)
+      {
+         string raptor_prompt_variable_zzyz;
+         ?? a;
+      
+         raptor_prompt_variable_zzyz ="which do you want";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         a= Double.Parse(Console.ReadLine());
+         func(a);
+      }
+   }
+}
